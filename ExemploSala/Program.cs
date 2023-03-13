03807c8d0dd076327464fead6d27f910f81b3d08
Aluno <Aluno@A202-431267>
2023-03-13 15:58:12 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploSala/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploSala
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante aluno1;
+            aluno1 = new Estudante();
+
+            aluno1.nome = "João Silva";
+            aluno1.RA = "1234X";
+            aluno1.notaBim1 = 8.5;
+            aluno1.notaBim2 = 9.0;
+
+            Console.WriteLine(aluno1.relatorio());
+        }
+    }
+
+}
---
+++ ExemploSala/estudante.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploSala
+{
+    internal class Estudante
+    {
+        public string RA;
+        public string nome;
+        public double notaBim1;
+        public double notaBim2;
+
+        public string relatorio()
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            sb.AppendFormat("Nome: {0}\n", nome);
+            sb.AppendFormat("RA: {0}\n", RA);
+            sb.AppendFormat("Nota 1º Bimestre {0}\n", notaBim1);
+            sb.AppendFormat("Nota 2º Bimestre {0}\n", notaBim2);
+            
+            return sb.ToString();
+        }
+    }
+}
